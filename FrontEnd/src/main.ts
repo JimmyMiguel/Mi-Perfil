521a6cf0fd91d4f9b8cb497990ccdb8891e0a24f
Jimmy <jimmymiguelgalarza@gmail.com>
2026-04-05 17:35:25 -0500

    Limpieza de formulario una vez se envia

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -5,6 +5,9 @@ container.innerHTML = `<my-perfil></my-perfil>`;
 
 // ✅ Esto funciona porque no hay Shadow DOM
 const form = container.querySelector('#perfilForm') as HTMLFormElement;
+const preview = container.querySelector('#preview') as HTMLElement;
+const previewImg = container.querySelector('#previewImg') as HTMLImageElement;
+const previewName = container.querySelector('#previewName') as HTMLElement;
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault(); // Evita que la página se recargue
@@ -38,7 +41,7 @@ form.addEventListener('submit', async (e) => {
     const dataCloudinary = await resCloudinary.json();
     const urlImagenLista = dataCloudinary.secure_url;
 
-    const resBackend = await fetch('http://localhost:3000/api/usuarios', {
+    const resBackend = await fetch('http://localhost:3000/perfil', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -52,6 +55,11 @@ form.addEventListener('submit', async (e) => {
 
     alert('Perfil guardado correctamente ✅');
     console.log('Éxito:', { nombre, biografia, urlImagenLista });
+
+    form.reset();
+    preview.style.display = 'none';
+    previewImg.src = '';
+    previewName.textContent = '';
 
   } catch (error) {
     console.error(error);
